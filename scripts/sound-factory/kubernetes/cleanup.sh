8c0a63653fab88c3416d841f6c24a32c71f1fe22
yehiyam <yehiyam@gmail.com>
2017-11-02 16:44:08 +0200

    update dockerfile to use node:8

--- scripts/sound-factory/kubernetes/cleanup.sh
+++ scripts/sound-factory/kubernetes/cleanup.sh
@@ -2,3 +2,5 @@
 kubectl delete -f pipeline-driver.yml
 kubectl delete -f api-server.yml
 kubectl delete -f worker-green.yml
+kubectl delete -f worker-yellow.yml
+kubectl delete -f worker-black.yml
--- scripts/sound-factory/kubernetes/start.sh
+++ scripts/sound-factory/kubernetes/start.sh
@@ -2,4 +2,6 @@
 kubectl apply -f pipeline-driver.yml
 kubectl apply -f api-server.yml
 kubectl apply -f worker-green.yml
+kubectl apply -f worker-yellow.yml
+kubectl apply -f worker-black.yml
 
