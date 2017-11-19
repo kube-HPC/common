6e8479eb9602eb49a1643f0d8dc418db9ee22a53
yehil <yehiyam@gmail.com>
2017-11-19 11:11:40 +0200

    fix yamls

--- scripts/kubernetes/cleanup.sh
+++ scripts/kubernetes/cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-kubectl delete -f yaml/pipeline-driver.yml
-kubectl delete -f yaml/webhook-stub-ui.yml
-kubectl delete -f yaml/api-server.yml
-kubectl delete -f yaml/worker-green.yml
-kubectl delete -f yaml/worker-yellow.yml
-kubectl delete -f yaml/worker-black.yml
+kubectl delete -f yaml/core/pipeline-driver.yml
+kubectl delete -f yaml/core/webhook-stub-ui.yml
+kubectl delete -f yaml/core/api-server.yml
+kubectl delete -f yaml/core/worker-green.yml
+kubectl delete -f yaml/core/worker-yellow.yml
+kubectl delete -f yaml/core/worker-black.yml
--- scripts/kubernetes/start.sh
+++ scripts/kubernetes/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-kubectl apply -f yaml/pipeline-driver.yml
-kubectl apply -f yaml/webhook-stub-ui.yml
-kubectl apply -f yaml/api-server.yml
-kubectl apply -f yaml/worker-green.yml
-kubectl apply -f yaml/worker-yellow.yml
-kubectl apply -f yaml/worker-black.yml
+kubectl apply -f yaml/core/pipeline-driver.yml
+kubectl apply -f yaml/core/webhook-stub-ui.yml
+kubectl apply -f yaml/core/api-server.yml
+kubectl apply -f yaml/core/worker-green.yml
+kubectl apply -f yaml/core/worker-yellow.yml
+kubectl apply -f yaml/core/worker-black.yml
 
